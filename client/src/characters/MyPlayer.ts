8a9bc6fed19aa2e9c1439e61f7f2e1be26e6e8cf
Stefan Nachtrab <stefan.nachtrab@adorsys.com>
2023-12-07 18:47:56 +0100

    fixed start position, screenshare and whiteboard share issues

--- client/src/characters/MyPlayer.ts
+++ client/src/characters/MyPlayer.ts
@@ -71,7 +71,7 @@ export default class MyPlayer extends Player {
           break
         case ItemType.VENDINGMACHINE:
           // hacky and hard-coded, but leaving it as is for now
-          const url = 'https://www.buymeacoffee.com/skyoffice'
+          const url = 'https://www.github.com/st3v0rr/SkyOffice'
           openURL(url)
           break
       }
